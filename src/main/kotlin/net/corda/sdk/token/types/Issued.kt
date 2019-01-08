df7999b474217c6bc29645a73880d6c2c931e9ab
Alex Koller <alex.koller@r3.com>
2019-01-08 16:17:24 +0000

    Adding missing annotation

--- src/main/kotlin/net/corda/sdk/token/types/Issued.kt
+++ src/main/kotlin/net/corda/sdk/token/types/Issued.kt
@@ -2,6 +2,7 @@ package net.corda.sdk.token.types
 
 import net.corda.core.contracts.TokenizableAssetInfo
 import net.corda.core.identity.Party
+import net.corda.core.serialization.CordaSerializable
 import net.corda.sdk.token.types.token.EmbeddableToken
 import java.math.BigDecimal
 
@@ -14,6 +15,8 @@ import java.math.BigDecimal
  * of the security would be implied via some of information contained within the token type state. E.g. a stock symbol
  * or a party object of the securities issuer, if they had a node on the network.
  */
+
+@CordaSerializable
 data class Issued<out T : EmbeddableToken>(val issuer: Party, val product: T) : TokenizableAssetInfo {
     override fun toString(): String = "$product issued by ${issuer.name.organisation}"
     override val displayTokenSize: BigDecimal get() = product.displayTokenSize
