45ba01b58d1438e7368821f23747691907b62526
Alex Koller <alex.koller@r3.com>
2019-01-08 17:56:47 +0000

    Making OwnedTokenAmountContract non-abstract

--- src/main/kotlin/net/corda/sdk/token/contracts/OwnedTokenAmountContract.kt
+++ src/main/kotlin/net/corda/sdk/token/contracts/OwnedTokenAmountContract.kt
@@ -3,7 +3,7 @@ package net.corda.sdk.token.contracts
 import net.corda.core.contracts.Contract
 import net.corda.core.transactions.LedgerTransaction
 
-abstract class OwnedTokenAmountContract : Contract {
+class OwnedTokenAmountContract : Contract {
 
     companion object {
         val contractId = this::class.java.enclosingClass.canonicalName
