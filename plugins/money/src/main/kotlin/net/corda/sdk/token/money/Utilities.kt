abf9a5c87b9587c0e065447a9cb164b5ac316d4d
RogerWillis <roger.willis@r3cev.com>
2019-01-23 15:54:48 +0000

    * Added CHF and YEN creation helpers to the money module.

--- plugins/money/src/main/kotlin/net/corda/sdk/token/money/Utilities.kt
+++ plugins/money/src/main/kotlin/net/corda/sdk/token/money/Utilities.kt
@@ -7,7 +7,6 @@ import net.corda.sdk.token.contracts.utilities.AMOUNT
 
 // Sterling.
 val GBP = FiatCurrency.getInstance("GBP")
-
 fun GBP(amount: Int): Amount<FiatCurrency> = AMOUNT(amount, GBP)
 fun GBP(amount: Long): Amount<FiatCurrency> = AMOUNT(amount, GBP)
 fun GBP(amount: Double): Amount<FiatCurrency> = AMOUNT(amount, GBP)
@@ -17,7 +16,6 @@ val Double.GBP: Amount<FiatCurrency> get() = GBP(this)
 
 // US Dollar.
 val USD = FiatCurrency.getInstance("USD")
-
 fun USD(amount: Int): Amount<FiatCurrency> = AMOUNT(amount, USD)
 fun USD(amount: Long): Amount<FiatCurrency> = AMOUNT(amount, USD)
 fun USD(amount: Double): Amount<FiatCurrency> = AMOUNT(amount, USD)
@@ -25,9 +23,8 @@ val Int.USD: Amount<FiatCurrency> get() = USD(this)
 val Long.USD: Amount<FiatCurrency> get() = USD(this)
 val Double.USD: Amount<FiatCurrency> get() = USD(this)
 
-// US Dollar.
-val EUR = FiatCurrency.getInstance("USD")
-
+// Euro.
+val EUR = FiatCurrency.getInstance("EUR")
 fun EUR(amount: Int): Amount<FiatCurrency> = AMOUNT(amount, EUR)
 fun EUR(amount: Long): Amount<FiatCurrency> = AMOUNT(amount, EUR)
 fun EUR(amount: Double): Amount<FiatCurrency> = AMOUNT(amount, EUR)
@@ -35,9 +32,28 @@ val Int.EUR: Amount<FiatCurrency> get() = EUR(this)
 val Long.EUR: Amount<FiatCurrency> get() = EUR(this)
 val Double.EUR: Amount<FiatCurrency> get() = EUR(this)
 
+// Swissie.
+val CHF = FiatCurrency.getInstance("CHF")
+
+fun CHF(amount: Int): Amount<FiatCurrency> = AMOUNT(amount, CHF)
+fun CHF(amount: Long): Amount<FiatCurrency> = AMOUNT(amount, CHF)
+fun CHF(amount: Double): Amount<FiatCurrency> = AMOUNT(amount, CHF)
+val Int.CHF: Amount<FiatCurrency> get() = CHF(this)
+val Long.CHF: Amount<FiatCurrency> get() = CHF(this)
+val Double.CHF: Amount<FiatCurrency> get() = CHF(this)
+
+// Japanese Yen.
+val JPY = FiatCurrency.getInstance("JPY")
+
+fun JPY(amount: Int): Amount<FiatCurrency> = AMOUNT(amount, JPY)
+fun JPY(amount: Long): Amount<FiatCurrency> = AMOUNT(amount, JPY)
+fun JPY(amount: Double): Amount<FiatCurrency> = AMOUNT(amount, JPY)
+val Int.JPY: Amount<FiatCurrency> get() = JPY(this)
+val Long.JPY: Amount<FiatCurrency> get() = JPY(this)
+val Double.JPY: Amount<FiatCurrency> get() = JPY(this)
+
 // Bitcoin.
 val BTC = DigitalCurrency.getInstance("BTC")
-
 fun BTC(amount: Int): Amount<DigitalCurrency> = AMOUNT(amount, BTC)
 fun BTC(amount: Long): Amount<DigitalCurrency> = AMOUNT(amount, BTC)
 fun BTC(amount: Double): Amount<DigitalCurrency> = AMOUNT(amount, BTC)
