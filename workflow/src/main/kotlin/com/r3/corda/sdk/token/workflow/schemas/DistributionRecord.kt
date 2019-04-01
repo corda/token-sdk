b53ca9a2e9edea69a324a2342f228d4fe5995476
Katarzyna Streich <katarzyna.streich@r3.com>
2019-04-01 16:13:49 +0100

    Add migration scripts

--- workflow/src/main/kotlin/com/r3/corda/sdk/token/workflow/schemas/DistributionRecord.kt
+++ workflow/src/main/kotlin/com/r3/corda/sdk/token/workflow/schemas/DistributionRecord.kt
@@ -2,13 +2,19 @@ package com.r3.corda.sdk.token.workflow.schemas
 
 import net.corda.core.identity.Party
 import net.corda.core.schemas.MappedSchema
+import org.hibernate.annotations.Type
 import java.util.*
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Index
+import javax.persistence.Table
 
-object DistributionRecordFamily
+object DistributionRecordSchema
 
-object DistributionRecordSchema : MappedSchema(
-        schemaFamily = DistributionRecordFamily.javaClass,
+object DistributionRecordSchemaV1 : MappedSchema(
+        schemaFamily = DistributionRecordSchema.javaClass,
         version = 1,
         mappedTypes = listOf(DistributionRecord::class.java)
 )
@@ -18,10 +24,11 @@ object DistributionRecordSchema : MappedSchema(
 class DistributionRecord(
 
         @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @GeneratedValue
         var id: Long,
 
         @Column(name = "linear_id", nullable = false)
+        @Type(type = "uuid-char")
         var linearId: UUID,
 
         @Column(name = "party", nullable = false)
