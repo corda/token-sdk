c1cc71fbf64c7b62aebb65ed7076b1131fe33c80
RogerWillis <roger.willis@r3cev.com>
2019-12-13 11:15:32 +0000

    Fixed ENT-4721 whereby when redeeming tokens from an account with the issuer, the issuer cannot request that the redeeming node generate a public key for the account that is redeeming tokens. We need to change the redeem flows such that if required, the redeeming node can pass the issuer the account ID for the account which is redeeming tokens. This will fix the issue.

--- workflows/src/main/kotlin/com/r3/corda/lib/tokens/workflows/flows/redeem/ConfidentialRedeemFungibleTokensFlowHandler.kt
+++ workflows/src/main/kotlin/com/r3/corda/lib/tokens/workflows/flows/redeem/ConfidentialRedeemFungibleTokensFlowHandler.kt
@@ -1,11 +1,8 @@
 package com.r3.corda.lib.tokens.workflows.flows.redeem
 
 import co.paralleluniverse.fibers.Suspendable
-import com.r3.corda.lib.ci.workflows.RequestKeyFlow
-import com.r3.corda.lib.tokens.workflows.internal.flows.finality.TransactionRole
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.FlowSession
-import net.corda.core.utilities.unwrap
 
 /**
  * Responder flow to [ConfidentialRedeemFungibleTokensFlow].
@@ -13,11 +10,6 @@ import net.corda.core.utilities.unwrap
 class ConfidentialRedeemFungibleTokensFlowHandler(val otherSession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
-        val role = otherSession.receive<TransactionRole>().unwrap { it }
-        if (role == TransactionRole.PARTICIPANT) {
-            subFlow(RequestKeyFlow(otherSession))
-        }
-        // Perform checks that the change owner is well known and belongs to the party that inititated the flow
         subFlow(RedeemTokensFlowHandler(otherSession))
     }
 }
