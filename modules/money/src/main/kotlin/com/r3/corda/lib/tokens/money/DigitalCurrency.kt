f4b38abcaa4b8e6ac21dd46e3c69b46c7e334968
rogers.a.nick <rogers.a.nick@gmail.com>
2020-03-10 23:06:07 -0400

    EG-685: Adjusting Fiat / Digital currency classes for easy access from Java code.

--- modules/money/src/main/kotlin/com/r3/corda/lib/tokens/money/DigitalCurrency.kt
+++ modules/money/src/main/kotlin/com/r3/corda/lib/tokens/money/DigitalCurrency.kt
@@ -3,8 +3,17 @@ package com.r3.corda.lib.tokens.money
 import com.r3.corda.lib.tokens.contracts.types.TokenType
 import java.util.*
 
-/** A representation of digital money. This implementation somewhat mirrors that of [Currency]. */
-class DigitalCurrency {
+/**
+ * A representation of digital money. This implementation somewhat mirrors that of [Currency].
+ *
+ * Note that the primary constructor only exists for simple and convenient access from Java code.
+ *
+ * @param currencyCode The currency code that represents the TokenType which the developer wishes to instantiate.
+ */
+class DigitalCurrency(currencyCode: String): TokenType(
+    getInstance(currencyCode).tokenIdentifier,
+    getInstance(currencyCode).fractionDigits
+) {
     companion object {
         private val registry = mapOf(
                 Pair("XRP", TokenType("Ripple", 6)),
---
+++ workflows/src/test/java/com/r3/corda/lib/tokens/workflows/java/CurrencyAccessFromJavaTest.java
@@ -0,0 +1,48 @@
+package com.r3.corda.lib.tokens.workflows.java;
+
+import com.r3.corda.lib.tokens.contracts.types.TokenType;
+import com.r3.corda.lib.tokens.money.DigitalCurrency;
+import com.r3.corda.lib.tokens.money.FiatCurrency;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CurrencyAccessFromJavaTest {
+
+    /**
+     * Sanity check for easy access of digital currency TokenTypes from java code
+     */
+    @Test
+    public void javaWrappedDigitalCurrencyIsIdenticalToKotlinCompanionObject() throws Exception {
+
+        List<String> digitalCurrencies = Arrays.asList("XRP", "BTC", "ETH", "DOGE");
+
+        digitalCurrencies.forEach((currencyCode) -> {
+            TokenType kotlinFiat = DigitalCurrency.Companion.getInstance(currencyCode);
+            TokenType javaWrappedFiat = new DigitalCurrency(currencyCode);
+
+            assert(kotlinFiat.getTokenIdentifier().equals(javaWrappedFiat.getTokenIdentifier()));
+            assert(kotlinFiat.getFractionDigits() == javaWrappedFiat.getFractionDigits());
+        });
+    }
+
+    /**
+     * Sanity check for easy access of fiat currency TokenTypes from java code
+     */
+    @Test
+    public void javaWrappedFiatCurrencyIsIdenticalToKotlinCompanionObject() throws Exception {
+
+        List<String> fiatCurrencies = Arrays.asList("GBP", "USD", "EUR", "CHF", "JPY", "CAD", "AUD", "NZD");
+
+        fiatCurrencies.forEach((currencyCode) -> {
+            TokenType kotlinFiat = FiatCurrency.Companion.getInstance(currencyCode);
+            TokenType javaWrappedFiat = new FiatCurrency(currencyCode);
+
+            assert(kotlinFiat.getTokenIdentifier().equals(javaWrappedFiat.getTokenIdentifier()));
+            assert(kotlinFiat.getFractionDigits() == javaWrappedFiat.getFractionDigits());
+        });
+    }
+
+}
+
