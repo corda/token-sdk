e5d883c880bed132b84c17103d0804f12bc8213b
rogers.a.nick <rogers.a.nick@gmail.com>
2020-03-17 11:12:46 -0400

    EG-710: Create a builder utility

---
+++ workflows/src/main/kotlin/com/r3/corda/lib/tokens/workflows/utilities/TokenBuilderUtility.kt
@@ -0,0 +1,9 @@
+package com.r3.corda.lib.tokens.workflows.utilities
+
+/**
+ * This is a utility class designed for Java developers to more easily
+ * access Kotlin infix functions to build token types.
+ */
+class TokenBuilderUtility() {
+
+}
